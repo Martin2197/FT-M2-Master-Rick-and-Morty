c78a810e6c6530f03c0a013a80700c44ffecf0ab
Martin2197 <martin.villalba2197@gmail.com>
2023-07-31 00:09:12 -0300

    Realizando Homework

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -3,15 +3,36 @@ import React from "react";
 import Animals from "../Animals/Animals";
 // eslint-disable-next-line no-unused-vars
 import Species from "../Species/Species";
-// import styledZoo from "./Zoo.module.css";
+
+import styledZoo from "./Zoo.module.css";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  const [zoo, setZoo] = React.useState({
+    zooName: '',
+    animals: [],
+    species: [],
+    allAnimals: []
+ });
 
+ function handleInputChange(event){
+    setZoo({
+      ...zoo, 
+      [event.target.name]: event.target.value
+    })
+ }
 
   return (
     <div>
-      <h1>-</h1>
+      <label>Zoo Name:</label>
+        <input 
+        name="zooName"
+        type="text" 
+        id="zooNameInput" 
+        value={zoo.zooName} 
+        onChange={handleInputChange}
+        />
+      <h1>{zoo.zooName}</h1>  
     </div>
   );
 }
